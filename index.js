a60a406f4ab669dc94fc5c863e74775cb59a06aa
sammychvz <sammy_chvz17@outlook.com>
2022-02-20 19:07:19 -0600

    added the prompt questions for all employees

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ const inquirer = require("inquirer");
 const path = require('Path'); 
 const fs = requirer('fs');
 
+const questions = [
+    { 
+        type: "input", 
+        name: "name", 
+        message: "What is the employee name?", 
+    },
+    {
+        type: "input",
+        name: "empId",
+        message: "What is the employee's ID number?", 
+    },
+    {
+        type: "input", 
+        name: "email", 
+        message: "What is the employee's email?",
+    },
+    {
+        type: "list", 
+        name: "role", 
+        message: "What is the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }, 
+];

974c8ed00137385ca83a3e4e62896a821027cae7
sammychvz <sammy_chvz17@outlook.com>
2022-02-20 18:56:55 -0600

    Added the const in the index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Engineer = require('./lib/Engineer');
+const Manager = require("./lib/Manager"); 
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require('Path'); 
+const fs = requirer('fs');
+

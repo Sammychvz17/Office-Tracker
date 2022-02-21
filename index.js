619cd89a06898e27cb5ae2b6fd1a25e299692764
sammychvz <sammy_chvz17@outlook.com>
2022-02-21 08:32:42 -0600

    added the js page where it puts together the html

--- index.js
+++ index.js
@@ -84,7 +84,12 @@ const getTeam = () => {
                     });
                 }
             });
-        });
+        })
+        .catch((err) => {
+            if (err) {
+              throw err;
+            }
+    });
 };
 
 getTeam();

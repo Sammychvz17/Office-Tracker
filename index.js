2c6e37e4fbe2ec5083bfd5d4c02c89f95f53057f
sammychvz <sammy_chvz17@outlook.com>
2022-02-20 19:29:56 -0600

    added the specific questions for certian roles and the option to add a new emp

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const inquirer = require("inquirer");
 const path = require('Path'); 
 const fs = requirer('fs');
 
+
 const questions = [
     { 
         type: "input", 
@@ -28,3 +29,38 @@ const questions = [
         choices: ["Manager", "Engineer", "Intern"]
     }, 
 ];
+
+const team = []; 
+const getTeam = () => {
+    inquirer 
+        .prompt(questions)
+        .then((inputOne) => {
+            inquirer 
+            .prompt ([
+                {
+                when: () => inputOne.role === "Manager",
+                type: "input", 
+                name: "officeNum", 
+                message: "What is the office number?",
+                },
+                {
+                    when: () => inputOne.role === "Engineer", 
+                    type: "input", 
+                    name: "github",
+                    message: "What is the Employees Github Username?",
+                },   
+                {
+                    when: () => inputOne.role === "Intern",
+                    type: "input", 
+                    name: "university",
+                    message: "What is the student university?", 
+                }, 
+                {
+                    type: "confirm", 
+                    name: "anotherEmp",
+                    Message: "Would you like to add another employee?",
+
+                },
+            ])
+        });
+}
